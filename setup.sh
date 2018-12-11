2a5fee4da91bd475b552c1121b990d9b4fd9367c
Zac Monroe <zmonroe0820@gmail.com>
2018-12-11 09:41:53 -0500

    Added brightness changing files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Install this in your home directory (~, $HOME)
-ln -s ~/dot/.vimrc  ~/.vimrc
-ln -s ~/dot/.bashrc ~/.bashrc
-ln -s ~/dot/config  ~/.i3/config
+ln -s ~/dot/.vimrc             ~/.vimrc
+ln -s ~/dot/.bashrc            ~/.bashrc
+ln -s ~/dot/config             ~/.i3/config
+ln -s ~/dot/.bup               ~/.bup
+ln -s ~/dot/.bdown             ~/.bdown
+ln -s ~/dot/.currentbrightness ~/.currentbrightness
