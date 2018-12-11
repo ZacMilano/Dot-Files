3b9f43b315ebc6d24955fa951394abd7d06a0961
Zac Monroe <zmonroe0820@gmail.com>
2018-12-11 10:00:12 -0500

    Added option to only install ssh files

---
+++ .vim/.vim
@@ -0,0 +1,1 @@
+/u/zcmonroe/dot/.vim
--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 # Install this in your home directory (~, $HOME)
-ln -s ~/dot/.vimrc             ~/.vimrc
 ln -s ~/dot/.vim               ~/.vim
-ln -s ~/dot/.bashrc            ~/.bashrc
-ln -s ~/dot/config             ~/.i3/config
-ln -s ~/dot/.bup               ~/.bup
-ln -s ~/dot/.bdown             ~/.bdown
-ln -s ~/dot/.currentbrightness ~/.currentbrightness
+
+while true; do
+  read -p "Installing on ssh?" yn
+  case $yn in
+    [Yy]* ) ln -s ~/dot/.vimrc_ssh ~/.vimrc; ln -s ~/dot/.bashrc ~/.bashrc;
+      break;;
+    [Nn]* ) ln -s ~/dot/.vimrc ~/.vimrc; ln -s ~/dot/.bashrc ~/.bashrc;
+      ln -s ~/dot/.bup ~/.bup; ln -s ~/dot/.bdown ~/.bdown;
+      ln -s ~/dot/.currentbrightness ~/.currentbrightness; break;;
+    * ) "Answer y or n.";;
+  esac
+done
+# ln -s ~/dot/.vimrc             ~/.vimrc
+# ln -s ~/dot/.bashrc            ~/.bashrc
+# ln -s ~/dot/config             ~/.i3/config
+# ln -s ~/dot/.bup               ~/.bup
+# ln -s ~/dot/.bdown             ~/.bdown
+# ln -s ~/dot/.currentbrightness ~/.currentbrightness
